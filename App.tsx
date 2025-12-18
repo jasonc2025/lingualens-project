f4c20bbcaa1a565318c5212b3c9cf689b67ed183
jasonc <jasoncmait@gmail.com>
2025-12-18 05:13:14 +0000

    Update App.tsx
    add user client

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useCallback } from 'react';
 import { Uploader } from './components/Uploader';
 import { AnnotationOverlay } from './components/AnnotationOverlay';
